d9049b4366ae72e669d25387a38c093f8fa115a7
Andy Bayer Roswell <andyroswellrr@gmail.com>
2024-11-27 00:43:58 +0100

    added vreapi deployment

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+helm upgrade --install --cleanup-on-fail jh jupyterhub/jupyterhub --version=3.2.1 --values rstudio-on-jh/helm/config.yaml
+kubectl apply -f rstudio-on-jh/configmap.yaml
+kubectl create secret generic secret-naavre-rstudio --from-literal=NAAVRE_API_TOKEN=${NAAVRE_API_TOKEN}
+helm upgrade --install paas oci://ghcr.io/qcdis/charts/vrepaas -f vreapi/helm/config.yaml
